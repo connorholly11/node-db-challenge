0c3518a3d3ddad95deb78941ba1d31507f2a2fe3
Connor Holly <connorholly11@gmail.com>
2019-10-11 13:57:57 -0400

    stretch seed data and update,delete projects... update,delete tasks not working

--- data/projects/projects.model.js
+++ data/projects/projects.model.js
@@ -3,9 +3,14 @@ const db = require("../../db.config");
 module.exports = {
   getProjects,
   getProjectsId,
-  addProject,
   getTasks,
-  addTasks
+  addProject,
+  getAllTasks,
+  addTasks,
+  updateProject,
+  updateTask,
+  deleteTask,
+  deleteProject
 };
 
 function getProjects() {
@@ -16,15 +21,22 @@ function getProjectsId(id) {
   return db("project").where("id", "=", id);
 }
 
-function addProject(project) {
-  return db("project").insert(project);
-}
-
 function getTasks(project_id) {
   return db("task")
     .join("project", "task.project_id", "=", "project.id")
     .select("project.*", "task.*")
     .where("project_id", "=", project_id);
+}
+
+//NOT WORKING
+function getAllTasks() {
+  console.log("all tasks is getting called");
+  console.log(db("task"));
+  return db("task");
+}
+
+function addProject(project) {
+  return db("project").insert(project);
 }
 
 function addTasks(task, project_id) {
@@ -33,3 +45,24 @@ function addTasks(task, project_id) {
     .insert(task)
     .where("project_id", "=", project_id);
 }
+
+function updateProject(changes, project_id) {
+  return db("project")
+    .update(changes)
+    .where("id", "=", project_id);
+}
+
+//NOT WORKING
+function updateTask(changes, project_id) {
+  return db("task")
+    .update(changes)
+    .where("id", "=", project_id);
+}
+
+function deleteProject(project_id) {
+  return db("project")
+    .del()
+    .where("id", "=", project_id);
+}
+
+function deleteTask(project_id) {}
--- data/projects/projects.router.js
+++ data/projects/projects.router.js
@@ -31,21 +31,6 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
-  const project = req.body;
-
-  db.addProject(project)
-    .then(project => {
-      res.status(201).json(project);
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: error,
-        message: "500 error on adding project"
-      });
-    });
-});
-
 router.get("/:id/tasks", (req, res) => {
   const id = req.params.id;
 
@@ -57,6 +42,36 @@ router.get("/:id/tasks", (req, res) => {
       res.status(500).json({
         error: error,
         message: "500 error on getting tasks"
+      });
+    });
+});
+
+//NOT WORKING
+router.get("/tasks", (req, res) => {
+  console.log("im getting all tasks");
+  db.getAllTasks()
+    .then(tasks => {
+      res.status(200).json(tasks);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+        message: "500 error on getting all tasks"
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  const project = req.body;
+
+  db.addProject(project)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+        message: "500 error on adding project"
       });
     });
 });
@@ -76,5 +91,56 @@ router.post("/:id/tasks", (req, res) => {
       });
     });
 });
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+
+  db.updateProject(changes, id)
+    .then(updated => {
+      res.status(200).json(updated);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+        message: "500 error on updating projects"
+      });
+    });
+});
+
+//NOT WORKING
+router.put("/:id/tasks", (req, res) => {
+  const changes = req.body;
+  const id = req.params.id;
+
+  db.updateTask(changes, id)
+    .then(task => {
+      res.status(200).json(task);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+        message: "500 error on updating tasks"
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.deleteProject(id)
+    .then(deleted => {
+      res.status(204).json(deleted);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+        message: "500 error on deleting project"
+      });
+    });
+});
+
+//NOT WORKING
+router.delete("/:id/tasks", (req, res) => {});
 
 module.exports = router;
