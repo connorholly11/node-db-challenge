03f8709d3e35d822a996e644a44b2cc3236f42e4
Connor Holly <connorholly11@gmail.com>
2019-10-11 12:20:42 -0400

    get projects works

--- data/projects/projects.router.js
+++ data/projects/projects.router.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const db = require("./projects.model");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("im in the projects router");
+  db.getProjects()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+        message: "500 error on getting projects"
+      });
+    });
 });
 
 module.exports = router;
